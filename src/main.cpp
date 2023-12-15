17ab6d775f15473f69b3d55e6daa8b6fc7211b0e
Ulfin85 <92341475+Ulfin85@users.noreply.github.com>
2023-12-15 00:34:38 -0500

    Lili make it work

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,29 @@
 #include <stepper.h>
 
 stepper Joint1;
+stepper Joint2;
+stepper Joint3;
+stepper Joint4;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Joint1.init(4, 2, 3, 1.8, 1);
+  Joint1.init(4, 2, 3, 1.8, 4);
+  Joint2.init(4, 5, 7, 1.8, 25);
+  Joint3.init(4, 8, 9, 1.8, 4);
+  Joint4.init(4, 10, 11, 0.9);
+
 
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-  Joint1.driveStepper(90);
-  delay(2000);
-  Joint1.getDeg();
-  Joint1.driveStepper(0);
-  delay(2000);
-  Joint1.getDeg();
-  Joint1.driveStepper(-90);
-  delay(2000);
-  Joint1.getDeg();
-  Joint1.driveStepper(-30);
-  delay(2000);
-  Joint1.getDeg();
-  Joint1.driveStepper(80);
-  delay(2000);
-  Joint1.getDeg();
-  Joint1.driveStepper(-20);
-  delay(2000);
-  Joint1.getDeg();
+  delay(15000);
+  for(int x = 0; x < 50; x++){
+    
+  }
   
 
-
-
-  
-  
-  
 
 }
